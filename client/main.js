ae22714f91943c9d4596a48da80549814e3cc058
Lucas Ocon <lucas_masterclas@hotmail.com>
2016-02-16 10:58:49 -0300

    Add routing & improve views

--- client/main.js
+++ client/main.js
@@ -1,3 +1,37 @@
+// Router
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+})
+
+Router.route('/', function() {
+  this.render('welcome', {
+    to: "main"
+  });
+});
+
+Router.route('/images', function() {
+  this.render('navbar', {
+    to: "navbar"
+  });
+  this.render("images", {
+    to: "main"
+  });
+});
+
+Router.route('/images/:_id', function() {
+  this.render('navbar', {
+    to: "navbar"
+  });
+  this.render('image', {
+    to: 'main',
+    data: function() {
+      return Images.findOne({_id: this.params._id})
+    }
+  })
+})
+
+
+// Infiniscroll
 Session.set("imagesLimit", 8);
 
 lastScrollTop = 0;
@@ -16,6 +50,8 @@ $(window).scroll(function(event) {
   }
 });
 
+
+// Accounts config
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_AND_EMAIL"
 });
